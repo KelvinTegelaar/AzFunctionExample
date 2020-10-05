676bd7569dd78d141e24b2ee660410b81a581812
Kelvin Tegelaar 💻 Lime Networks <kelvin@limenetworks.nl>
2020-10-05 09:38:17 +0200

    First commit

---
+++ HTTPS/run.ps1
@@ -0,0 +1,11 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = "Hi! You triggered a script via HTTPs."
+})
